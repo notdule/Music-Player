3b8a46d5de4693374ceea5c94964cb40ef367802
notdule <148584497+notdule@users.noreply.github.com>
2024-04-12 23:28:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -103,10 +103,18 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
-  
+
   playButton.classList.remove("playing");
   audio.pause();
 };
+
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+  }
+  };
 
 const renderSongs = (array) => {
   const songsHTML = array
@@ -130,9 +138,7 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-const getCurrentSongIndex = () => {
-
-}
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
     if (userData?.currentSong === null) {
