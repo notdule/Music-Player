6e441918077e6ec176bcd7ac130c44029847dad9
notdule <148584497+notdule@users.noreply.github.com>
2024-04-22 23:32:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {
+const playPreviousSong = () =>{
    if (userData?.currentSong === null) return;
    else {
     const currentSongIndex = getCurrentSongIndex();
@@ -155,14 +155,17 @@ const deleteSong = (id) => {
   userData.songs = userData?.songs.filter((song) => song.id !== id);
   renderSongs(userData?.songs); 
   highlightCurrentSong(); 
-  setPlayButtonAccessibleText();
+  setPlayButtonAccessibleText(); 
 
   if (userData?.songs.length === 0) {
     const resetButton = document.createElement("button");
-  
-  }
+    const resetText = document.createTextNode("Reset Playlist");
 
-  };
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    }
+
+};
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
